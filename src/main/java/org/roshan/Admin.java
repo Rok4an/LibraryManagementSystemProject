e3081acf33dd279eec64561a3965fdef6662d450
Roshan <roshansamyla@gmail.com>
2026-05-14 17:27:42 -0400

    Added more conditions to the report and added a toCSV

--- src/main/java/org/roshan/Admin.java
+++ src/main/java/org/roshan/Admin.java
@@ -1,5 +1,6 @@
 package org.roshan;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -9,14 +10,13 @@ import java.util.List;
 @Getter
 @Setter
 @ToString(callSuper = true)
-
+@EqualsAndHashCode(callSuper = true)
 public class Admin extends User implements Reportable {
-    private String  adminId;
+    private String adminId;
 
     public Admin(String name, Gender gender)
             throws InvalidNameException, InvalidIdException {
         super(name, gender);
-
         this.adminId = String.format("%06d", nextId++);
         this.userId = "A" + this.adminId;
 
@@ -26,20 +26,58 @@ public class Admin extends User implements Reportable {
     }
 
     @Override
-    protected int getBorrowLimit() {
+    public int getBorrowLimit() {
         return Integer.MAX_VALUE;
     }
 
     @Override
     protected void validateBorrow(Item item) {
-        // Can borrow any item
+        // Admins can borrow any item type
     }
 
+    /**
+     * Generates a report that shows every item grouped by status.
+     * @param items the list of items to include in the report
+     */
     @Override
     public void generateReport(List<Item> items) {
-        System.out.println("=== LIBRARY REPORT ===");
-        for (Item item : items) {
-            System.out.println(item.getStatus() + " | " + item);
+        System.out.println("LIBRARY STATUS REPORT\n");
+
+        System.out.println("BORROWED ITEMS:");
+        List<Item> borrowedItems = items.stream()
+                .filter(item -> item.getStatus() == Item.ItemStatus.BORROWED)
+                .distinct()
+                .toList();
+        System.out.println(borrowedItems);
+
+        System.out.println("\nAVAILABLE ITEMS:");
+        List<Item> availableItems = items.stream()
+                .filter(item -> item.getStatus() == Item.ItemStatus.AVAILABLE)
+                .distinct()
+                .toList();
+        System.out.println(availableItems);
+
+        System.out.println("\nLOST ITEMS:");
+        List<Item> lostItems = items.stream()
+                .filter(item -> item.getStatus() == Item.ItemStatus.LOST)
+                .distinct()
+                .toList();
+        System.out.println(lostItems);
+    }
+
+    /**
+     * Converts information to CSV format.
+     * @return a string of information in CSV format
+     */
+    public String toCSV() {
+        String borrowed = "";
+        for (int i = 0; i < borrowedItems.size(); i++) {
+            Item item = borrowedItems.get(i);
+            borrowed += item.getId();
+            if (i < borrowedItems.size() - 1) {
+                borrowed += ";";
+            }
         }
+        return userId + "," + name + ",ADMIN," + gender + "," + borrowed + ",,";
     }
 }
